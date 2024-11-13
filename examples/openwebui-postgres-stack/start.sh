ba6611c851feda583c72c44c751388771d63c0d5
Mgs <akerberganders@gmail.com>
2024-11-13 22:59:33 +0100

    Fix host mode (#43)
    * fix bundled and keep images for prunE
    
    * made openwebui into a port
    
    * making the port update for ollama access for continue
    
    * fix conflict in .env.example
    
    * Fix network host mode
    
    * Fix separate ollama instance from openwebui

--- examples/openwebui-postgres-stack/start.sh
+++ examples/openwebui-postgres-stack/start.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
-
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$SCRIPT_DIR" || exit
 
 KEY_FILE=.webui_secret_key
-
 PORT="${PORT:-8080}"
 HOST="${HOST:-0.0.0.0}"
 if test "$WEBUI_SECRET_KEY $WEBUI_JWT_SECRET_KEY" = " "; then
@@ -20,65 +18,21 @@ if test "$WEBUI_SECRET_KEY $WEBUI_JWT_SECRET_KEY" = " "; then
   WEBUI_SECRET_KEY=$(cat "$KEY_FILE")
 fi
 
-if [[ "${USE_OLLAMA_DOCKER,,}" == "true" ]]; then
-    echo "USE_OLLAMA is set to true, starting ollama serve."
-    ollama serve &
+WEBUI_SECRET_KEY="$WEBUI_SECRET_KEY" uvicorn open_webui.main:app --host "$HOST" --port "$PORT" --forwarded-allow-ips '*' &
+webui_pid=$!
+echo "Waiting for webui to start..."
+while ! curl -s http://localhost:8080/health > /dev/null; do
+  sleep 1
+done
+echo "Creating admin user..."
+curl \
+  -X POST "http://localhost:8080/api/v1/auths/signup" \
+  -H "accept: application/json" \
+  -H "Content-Type: application/json" \
+  -d "{ \"email\": \"${ADMIN_USER_EMAIL}\", \"password\": \"${ADMIN_USER_PASSWORD}\", \"name\": \"Admin\" }"
+echo "Shutting down webui..."
+kill $webui_pid
 
-    echo "Admin user configured, creating"
-    WEBUI_SECRET_KEY="$WEBUI_SECRET_KEY" uvicorn open_webui.main:app --host "$HOST" --port "$PORT" --forwarded-allow-ips '*' &
-    webui_pid=$!
-    echo "Waiting for webui to start..."
-    while ! curl -s http://localhost:8080/health > /dev/null; do
-      sleep 1
-    done
-    echo "Creating admin user..."
-    curl \
-      -X POST "http://localhost:8080/api/v1/auths/signup" \
-      -H "accept: application/json" \
-      -H "Content-Type: application/json" \
-      -d "{ \"email\": \"${ADMIN_USER_EMAIL}\", \"password\": \"${ADMIN_USER_PASSWORD}\", \"name\": \"Admin\" }"
-    echo "Shutting down webui..."
-    kill $webui_pid
-fi
-
-if [[ "${USE_CUDA_DOCKER,,}" == "true" ]]; then
-  echo "CUDA is enabled, appending LD_LIBRARY_PATH to include torch/cudnn & cublas libraries."
-  export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/lib/python3.11/site-packages/torch/lib:/usr/local/lib/python3.11/site-packages/nvidia/cudnn/lib"
-fi
-
-# Check if SPACE_ID is set, if so, configure for space
-if [ -n "$SPACE_ID" ]; then
-  echo "Configuring for HuggingFace Space deployment"
-  if [ -n "$ADMIN_USER_EMAIL" ] && [ -n "$ADMIN_USER_PASSWORD" ]; then
-    echo "Admin user configured, creating"
-    WEBUI_SECRET_KEY="$WEBUI_SECRET_KEY" uvicorn open_webui.main:app --host "$HOST" --port "$PORT" --forwarded-allow-ips '*' &
-    webui_pid=$!
-    echo "Waiting for webui to start..."
-    while ! curl -s http://localhost:8080/health > /dev/null; do
-      sleep 1
-    done
-    echo "Creating admin user..."
-    curl \
-      -X POST "http://localhost:8080/api/v1/auths/signup" \
-      -H "accept: application/json" \
-      -H "Content-Type: application/json" \
-      -d "{ \"email\": \"${ADMIN_USER_EMAIL}\", \"password\": \"${ADMIN_USER_PASSWORD}\", \"name\": \"Admin\" }"
-    echo "Shutting down webui..."
-    kill $webui_pid
-  fi
-
-  export WEBUI_URL=${SPACE_HOST}
-fi
-
-if [[ "${USE_OLLAMA_DOCKER,,}" == "true" ]]; then
-   MODELS_ARRAY=($(echo $MODELS | jq -r '.[]'))
-   echo "Installing default models."
-   for model in "${MODELS_ARRAY[@]}"; do
-        echo "Installing model $model."
-        ollama run "$model" &
-    done
-    echo "Done installing default models."
-fi
 
 
 WEBUI_SECRET_KEY="$WEBUI_SECRET_KEY" exec uvicorn open_webui.main:app --host "$HOST" --port "$PORT" --forwarded-allow-ips '*'
