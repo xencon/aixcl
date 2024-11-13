f22fe8c49a1c95971af22a285cb3b2be883c05f6
Mgs <akerberganders@gmail.com>
2024-11-13 22:56:59 +0100

    Fix separate ollama instance from openwebui

---
+++ examples/openwebui-postgres-stack/entrypoint.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Start Ollama in the background.
+/bin/ollama serve &
+# Record Process ID.
+pid=$!
+
+# Install jq
+if ! command -v jq &> /dev/null
+then
+    echo "jq could not be found, installing..."
+    apt-get update && apt-get install -y jq
+else
+    echo "jq is already installed."
+fi
+
+# Pause for Ollama to start.
+sleep 5
+
+MODELS_ARRAY=($(echo $MODELS | jq -r '.[]'))
+echo "Installing default models."
+echo $MODELS
+for model in "${MODELS_ARRAY[@]}"; do
+    echo "Installing model $model."
+    ollama run "$model" &
+done
+echo "Done installing default models."
+
+# Wait for Ollama process to finish.
+wait $pid
