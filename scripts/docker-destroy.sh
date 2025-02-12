4b25548a3517621a8a754cfc6b4db4ad2fd2b359
Salim Badakhchani <sbadakhc@gmail.com>
2025-02-12 21:15:33 +0100

    fix volume removal

--- scripts/docker-destroy.sh
+++ scripts/docker-destroy.sh
@@ -18,32 +18,39 @@ print_message() {
 # Function to delete all Docker containers
 delete_containers() {
     print_message "Deleting all Docker containers..." "yellow"
-    docker rm -f $(docker ps -a -q) 2>/dev/null
+    docker ps -aq | xargs -r docker rm -f 2>/dev/null
 }
 
 # Function to delete all Docker images
 delete_images() {
     print_message "Deleting all Docker images..." "yellow"
-    docker rmi -f $(docker images -q) 2>/dev/null
+    docker images -q | xargs -r docker rmi -f 2>/dev/null
 }
 
 # Function to delete all Docker networks
 delete_networks() {
     print_message "Deleting all Docker networks..." "yellow"
-    docker network prune -f
+    docker network ls -q | xargs -r docker network rm 2>/dev/null
 }
 
 # Function to delete all Docker volumes
 delete_volumes() {
     print_message "Deleting all Docker volumes..." "yellow"
-    docker volume prune -f
+    # Stop all containers first to release volume locks
+    docker ps -aq | xargs -r docker stop 2>/dev/null
+    # Remove containers again to ensure all are gone
+    docker ps -aq | xargs -r docker rm -f 2>/dev/null
+    # Remove all volumes forcefully
+    docker volume ls -q | xargs -r docker volume rm -f 2>/dev/null
+    # Final cleanup with system prune
+    docker system prune -a --volumes -f >/dev/null 2>&1
 }
 
-# Execute deletion functions
+# Execute deletion functions in the correct order
 delete_containers
-delete_images
 delete_networks
 delete_volumes
+delete_images
 
 # Verification
 print_message "Verifying deletion of Docker artifacts..." "blue"
