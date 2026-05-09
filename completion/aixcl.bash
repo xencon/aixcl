d05c441b8466581386b4be6faa45786bda939015
Salim Badakhchani <sbadakhc@gmail.com>
2026-05-09 15:40:23 +0100

    Fix bash completion offering clean instead of prune (#1097) (#1098)
    Update utils subcommand completion from 'clean' to 'prune' to match
    the rename introduced in #1091.

--- completion/aixcl.bash
+++ completion/aixcl.bash
@@ -148,7 +148,7 @@ _aixcl_complete() {
             return 0
             ;;
         'utils')
-            local utils_actions="check-env bash-completion clean"
+            local utils_actions="check-env bash-completion prune"
             mapfile -t COMPREPLY < <(compgen -W "$utils_actions" -- "$cur")
             return 0
             ;;
